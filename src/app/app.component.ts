e7c6bd16f8ec519385d36205d3144f0b9f2e0939
xrr2016 <xiaoranran1993@163.com>
2017-07-07 22:58:08 +0800

    :beer:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,7 +76,7 @@ export class AppComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,4 +8,11 @@ export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES)
   }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(this.getHeroes()), 2000)
+    }) 
+  }
 }
+
