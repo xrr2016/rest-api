bfec8d260601338c25bdcb0832522f8094e8a193
xrr2016 <xiaoranran1993@163.com>
2017-07-08 23:22:44 +0800

    :green_heart:

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,26 @@
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes'
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
@@ -15,7 +30,11 @@ export class HeroService {
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
+}
 }
