b395d3b2cf09a60d15683b8a303b3600cb879a1b
xrr2016 <xiaoranran1993@163.com>
2017-07-09 22:12:52 +0800

    :art:

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes'
+  private headers = new Headers({ 'Content-type': 'application/json' })
 
   constructor(private http: Http) { }
 
@@ -36,5 +37,38 @@ export class HeroService {
       .then(response => response.json().data as Hero)
       .catch(this.handleError);
   }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`
+    return this.http
+      .put(url, JSON.stringify(hero), { headers: this.headers })
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError)
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name }), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError)
+  }
+
+  delete(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`
+    return this.http.delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError)
+  }
 }
-}
+
+
+
+
+
+
+
+
+
