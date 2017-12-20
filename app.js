8e7960a9b5d6b13f1876dcd7663caf2e4aa02186
xrr2016 <xiaoranran1993@163.com>
2017-12-20 23:18:14 +0800

    :bug: handling errors

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 
 const routes = require('./routes')
 
+app.use(morgan('dev'))
 app.use('/products', routes.productRoutes)
 app.use('/orders', routes.orderRoutes)
 
+
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: 'It works!'
+//   })
+// })
+
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
-  })
+  const err = new Error('Not found!')
+  err.status = 404
+  next(err)
 })
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({
+    error: {
+      message: err.message
+    }
+  })
+})
 module.exports = app
